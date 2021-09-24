f3b112a0b2f8d386d5e3415411a9508911f816a6
Sturion <sturion.douglas@gmail.com>
2021-09-24 19:22:42 -0300

    att

--- busca.js
+++ busca.js
@@ -24,15 +24,12 @@ const pokeSearch = () => {
                     pokemon.name
                   }" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
           pokemon.id
-        }.png"/>
+        }.png" onError="this.onerror=null;this.src='/assets/pokebola.png';"/>
                   
-                  <p class="card-subtitle ${types[0]}">${types[0]}</p>
-                  <p class="card-subtitle ${types[1]}">${types[1]}</p>
-                  
+                  <p class="card-subtitle ${types[0]}"><img class="typeImg" src="assets/${types[0]}.svg"/>${types[0]}</p>
+                  <p class="card-subtitle ${types[1]}"><img class="typeImg" src="assets/${types[1]}.svg"/>${types[1]}</p>
                   
                   </li>
-
-                  
                   `;
   
         return accumulator;
